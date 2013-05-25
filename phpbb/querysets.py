7be99fdc9e140044a00470f97b00091b5b976755
Maciej Bliziński <maciej.blizinski@gmail.com>
2013-05-25 13:50:54 +0200

    Move common querysets to a shared module.

---
+++ phpbb/querysets.py
@@ -0,0 +1,14 @@
+from django.conf import settings
+import phpbb.models
+
+# TODO: get rid of hardcoded forum IDs
+# They could be fixed by implementing permission tables
+# from phpbb, but this runs into bug 373.
+#
+# http://code.djangoproject.com/ticket/373
+#
+# More information:
+# http://code.djangoproject.com/wiki/MultipleColumnPrimaryKeys
+forumqs = phpbb.models.PhpbbForum.objects
+for excluded_forum_id in settings.PHPBB_BLOCKED_FORUMS:
+  forumqs = forumqs.exclude(forum_id=excluded_forum_id)
