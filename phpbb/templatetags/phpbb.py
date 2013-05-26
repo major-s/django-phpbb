e59ed7ff0cfba47ccb6e134054ea87eea51f7a05
Maciej Bliziński <maciej.blizinski@gmail.com>
2013-05-26 11:55:53 +0200

    Ditch own bbcode parser, use postmarkup.
    http://code.google.com/p/postmarkup/

--- phpbb/templatetags/phpbb.py
+++ phpbb/templatetags/phpbb.py
@@ -23,62 +23,18 @@ from django.template.defaultfilters import stringfilter
 from django import template
 from django.conf import settings
 
+# Requires http://code.google.com/p/postmarkup
+from postmarkup import render_bbcode
+
+from django.utils.safestring import mark_safe
+
 register = template.Library()
-
-
-SUBS = (
-        #lists
-        (r'\[\*:[^\]]+\](.*?)\n \[/\*:[^\]]+\]',r'<li>\1</li>'),
-        (r'\[\*:[^\]]+\](.*?)\[/\*:[^\]]+\]',r'<li>\1</li>'),
-        (r'\[list:[^\]]+\](.*?)\[/list:[^\]]+\]',r'<ul>\1</ul>'),
-        # URLS
-        (r'\[url=([^\]]*)\]([^\[]*)\[/url\]', r'<a href="\1">\2</a>'),
-        (r'\[url=([^\]]*):\w+\](.*?)\[/url:\w+\]', r'<a href="\1">\2</a>'),
-        (r'\[url\](.*?)\[/url\]', r'<a href="\1">\1</a>'),
-        # IMG
-        (r'\[img:\w+\]([^\[]*)\[/img:\w+\]', r'<img src="\1" />'),
-        # Smilies path
-        ('{SMILIES_PATH}', settings.PHPBB_SMILIES_URL),
-        # [u], [i], [b]
-        (r'\[b:[^\]]*\]([^\[]*)\[/b:[^\]]*\]', r'<b>\1</b>'),
-        (r'\[i:[^\]]*\]([^\[]*)\[/i:[^\]]*\]', r'<i>\1</i>'),
-        (r'\[u:[^\]]*\]([^\[]*)\[/u:[^\]]*\]', r'<u>\1</u>'), #DEPRECATED, FIXME
-
-#        (r'\[\*:[^\]]*\]([^\[]*)\[/i:[/\*^\]]*\]', r'<i>\1</i>'),
-        #quote
-        (r'\[quote:\w+\]([^\[]*)\[/quote:\w+\]', r'<blockquote>\1</blockquote>'),
-        (r'\[quote=&quot;(.+?)&quot;:\w+\](.*)\[/quote:\w+\]', r'<blockquote><strong>\1:</strong><br /> \2</blockquote>'),
-        (r'\[quote="(.+?)":\w+\](.*)\[/quote:\w+\]', r'<blockquote><strong>\1:</strong><br /> \2</blockquote>'),
-        # size
-        (r'\[size=([0-9]+):\w+\](.*)\[/size:\w+\]', r'<font size="\1">\2</font>'),
-        # Automatically convert http:// in url
-        (r'(\s)http://([\w,.?=%/-]+)', r'<a href="\2">\2</a>(\s)'),
-        # Return to the line
-        (r'\n', r'<br>\n'),
-        ## No idea, really
-        #(r'\?+', r'?'),
-        # Remove all tags, don't know why you would want that
-        #(r'<![^>]+>', r' '),
-        #(r'<img[^>]+>', r' '),
-        #(r'<a[^>]+>', r' '),
-        #(r'</[^>]+>', r' '),
-        )
-
-# Compiled subs
-CSUBS = [(re.compile(a,re.S),b) for a,b in SUBS]
 
 @register.filter
 @stringfilter
 def bbcode(s):
-    for pattern, replacement in CSUBS:
-        s = pattern.sub(replacement, s)
-
-    return s
-
-bbcode.is_safe = True
+    return mark_safe(render_bbcode(s))
 
 @register.filter
 def withlink(obj):
-    return "<a href=\"%s\">%s</a>" % (obj.get_absolute_url(), str(obj))
-
-
+    return "<a href=\"%s\" rel=\"nofollow\">%s</a>" % (obj.get_absolute_url(), str(obj))
